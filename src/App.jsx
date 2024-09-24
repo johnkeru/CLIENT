e5b8eb53d34c9edf6ed1f3f4e99a0eba37637ee8
john keru <keruk355@gmail.com>
2024-09-24 16:52:18 +0800

    index content

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,23 @@ const App = () => {
   const [message, setMessage] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:5000/')
-      .then(res => setMessage(res.data.message))
+    // fetch(
+    //   'http://localhost:5000/users',
+    //   { 
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({ name: 'John Doe' })
+    //    }
+    // )
+    // .then(res => res.json())
+    // .then(data => console.log(data))
+
+    // axios.post('http://localhost:5000/users', { name: 'John Doe' })
+    //   .then(data => console.log(data))
+
+    axios.get('http://localhost:5000/').then(res => setMessage(res.data.message))
   }, [])
 
   return (
