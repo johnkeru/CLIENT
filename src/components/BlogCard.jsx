69a835e39a2cd9149819e08d1da8ee59594ea0ea
john keru <keruk355@gmail.com>
2024-09-25 15:46:23 +0800

    display blog!

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -10,35 +10,9 @@ import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import { styled } from '@mui/material/styles';
 import * as React from 'react';
 
 // CTRL + SHIFT + P
-
-
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme }) => ({
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-    variants: [
-        {
-            props: ({ expand }) => !expand,
-            style: {
-                transform: 'rotate(0deg)',
-            },
-        },
-        {
-            props: ({ expand }) => !!expand,
-            style: {
-                transform: 'rotate(180deg)',
-            },
-        },
-    ],
-}));
 
 export default function BlogCard({ blog }) {
 
