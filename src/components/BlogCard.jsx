2198c78d87cd83d0eff083f0a35a4d7d649759e0
john keru <keruk355@gmail.com>
2024-09-26 12:57:36 +0800

    added likes logic

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -7,14 +7,39 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
+import { grey, pink, red } from '@mui/material/colors';
 import * as React from 'react';
 import formatDateTime from '../utility/formatDateTime';
 import BlogMenu from './BlogMenu';
+import api from '../configs/api';
+import { useUser } from '../context/UserContext'
 
 // CTRL + SHIFT + P
 
 export default function BlogCard({ blog, methods }) {
+
+    const { currentUser } = useUser()
+
+    const [likes, setLikes] = React.useState(0)
+    const [isLike, setIsLike] = React.useState(false)
+
+    const handleToggleLike = () => {
+        api.post(`/blogs/${blog._id}/like`)
+            .then(res => {
+                setLikes(res.data.likes)
+                setIsLike(res.data.isLike)
+            })
+    }
+
+    React.useEffect(() => {
+        setLikes(blog.likes.length)
+        blog.likes.forEach(userId => {
+            if (currentUser?._id === userId._id) {
+                setIsLike(true)
+            }
+        })
+    }, [])
+
     return (
         <Card sx={{ cursor: 'pointer', maxWidth: '100%', mb: 2, }}>
             <CardHeader
@@ -39,9 +64,10 @@ export default function BlogCard({ blog, methods }) {
                 </Typography>
             </CardContent> : undefined}
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
+                <IconButton aria-label="add to favorites" onClick={() => handleToggleLike()}>
+                    <FavoriteIcon sx={{ color: isLike ? pink[500] : grey[500] }} />
                 </IconButton>
+                {likes}
             </CardActions>
         </Card>
     );
