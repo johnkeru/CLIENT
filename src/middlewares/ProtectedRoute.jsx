262d19c7bf68188bed478be0e3bbd966efc0f78a
john keru <80145233+johnkeru@users.noreply.github.com>
2024-09-27 14:16:01 +0800

    Update ProtectedRoute.jsx

--- src/middlewares/ProtectedRoute.jsx
+++ src/middlewares/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useUser } from "../context/UserContext";
+import { useUser } from "../CONTEXT/UserContext";
 
 const ProtectedRoute = ({ children }) => {
     const { token } = useUser();
